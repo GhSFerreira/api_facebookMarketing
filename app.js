cf2c21beab200c6101e60c2f0c01b1924f910565
Gabriel Ferreira <gabrielferreira.one@gmail.com>
2023-02-22 15:45:14 -0300

    change ports

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const env = require('dotenv').config();
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT_APP || 3000;
 
 const {downloadAdset, downloadAds, downloadInsights, downloadAccountCampaigns} = require('./downloadFiles')
 
